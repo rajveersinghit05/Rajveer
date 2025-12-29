331d7a814bde7123636032c8cbce128bc53d5be3
Rajveer Singh <rajveersingh.it27@jecrc.ac.in>
2025-12-29 17:00:44 +0530

    Initial project commit

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+    TokenObtainPairView as LoginView,
     TokenRefreshView,
 )
 from .views import RegisterAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
